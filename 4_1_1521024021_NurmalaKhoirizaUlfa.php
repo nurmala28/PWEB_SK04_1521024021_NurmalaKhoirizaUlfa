26727217fb62fed31dd9ffa3523e03c7c2a9f5c3
nurmala28 <malaelek77@gmail.com>
2016-11-23 16:14:33 +0700

    PWEB_SK04_1521024021_NurmalaKhoirizaUlfa

---
+++ 4_1_1521024021_NurmalaKhoirizaUlfa.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Studi Kasus 4_1</title>
+</head>
+<body>
+
+	<?php
+	$a = 72;
+	if (($a>=0) && ($a<=59)) {
+		echo "Cukup";
+	} elseif (($a>=60) && ($a<=85)) {
+		echo "Baik";
+	} elseif (($a>=85) && ($a<=100)) {
+		echo "Baik Sekali";
+	} else {
+		echo "Input Salah";
+	}
+	?>
+	
+</body>
+</html>
